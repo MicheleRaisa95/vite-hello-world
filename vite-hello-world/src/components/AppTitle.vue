5d2774f6c93d31b0ec0b7afaf11a0b50c7f5e511
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-05-29 17:54:55 +0200

    Aggiunto titolo

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+
+<script>
+export default {
+  name: 'AppTitle'
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
